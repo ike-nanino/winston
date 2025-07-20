7b8c0b435d88eaa24f094d49b4f31fbccfc66ef2
ike-nanino <ikesemanu@gmail.com>
2025-07-20 20:41:44 +0000

    Update siteUrl in sitemap configuration to use placeholder domain

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,4 +1,4 @@
-@type {import('next-sitemap').IConfig} */
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://winstoncofirm.com', // replace with your actual domain
   generateRobotsTxt: true,
